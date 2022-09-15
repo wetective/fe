344f1b3289f6902d0ace62d275756b708aca8765
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-15 12:45:00 -0600

    Update routes; add locations

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/investigations/:id', to: 'investigations#show'
 
-  resources :maps, only: [:index, :show]
+  resources :locations, only: [:index, :show]
 end
