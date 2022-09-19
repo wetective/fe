8feeb507c5aba1271dc78f9b0d3312833d895c8c
Cory Bethune <corybbethune@gmail.com>
2022-09-19 01:11:22 -0700

    wip

--- app/controllers/investigations_controller.rb
+++ app/controllers/investigations_controller.rb
@@ -3,6 +3,7 @@ class InvestigationsController < ApplicationController
   def index
     @investigations = InvestigationFacade.create_investigations
     @investigations.delete(nil)
+    # binding.pry
   end
 
   def show
