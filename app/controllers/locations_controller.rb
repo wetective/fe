eb3448b80c359d01d811d07932e71eced7d4d7c8
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-19 09:08:09 -0600

    Fix map render; clean up locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,4 @@
 class LocationsController < ApplicationController
-  before_action :set_location
-
   def index
     @locations = LocationFacade.new.markers
     if params[:city]
@@ -10,27 +8,4 @@ class LocationsController < ApplicationController
       # @pagy, @investigations = pagy(@investigations, items: 15)
     end
   end
-
-  def results
-    facade = InvestigationFacade.new
-    @investigations = facade.cities_investigations(@city)
-    respond_to do |format|
-      format.html { redirect_to locations_path }
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("results", partial: "locations/results", locals: { investigations: @investigations, city: @city })
-      end
-      # format.html
-    end
-    # render partial: 'locations/results', locals: { investigations: @investigations, city: @city }
-    # respond_to do |format|
-    #   format.turbo_stream { 
-    #     render turbo_stream: turbo_stream.replace("investigations", partial: "locations/table", locals: { investigations: investigations, city: city })
-    #     }
-    # end
-  end
-
-  private
-    def set_location
-      @city = params[:filter] || nil
-    end
 end
