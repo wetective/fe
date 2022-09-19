271c67f6f90cd3a90952656f3a1f0ef1ddcfcb42
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-19 10:32:49 -0400

    Remove user, use from BE instead

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_114818) do
+ActiveRecord::Schema.define(version: 2022_09_15_184254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,14 +19,6 @@ ActiveRecord::Schema.define(version: 2022_09_19_114818) do
     t.string "city"
     t.string "state"
     t.point "lat_long"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.integer "uid"
-    t.string "email"
-    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
