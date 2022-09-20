8539f8e025a335ee4c24436900fa1880c0f86de2
Cory Bethune <corybbethune@gmail.com>
2022-09-20 14:03:16 -0700

    Carousel works and most of the images render.

--- app/facades/investigation_facade.rb
+++ app/facades/investigation_facade.rb
@@ -2,7 +2,6 @@ class InvestigationFacade
 
   def self.create_investigations
     investigations = InvestigationService.get_investigations
-    binding.pry
     investigations.map do |investigation|
       if investigation[:status] != 'captured'
         Investigation.new(investigation)
--- app/services/investigation_service.rb
+++ app/services/investigation_service.rb
@@ -8,15 +8,15 @@ class InvestigationService
     response = conn.get("wanted/v1/list")
     total_investigations = JSON.parse(response.body, symbolize_names: true)[:total]
     all_data = []
+    i = 1
 
     until all_data.count >= total_investigations
-      i = 1
       response_2 = conn.get("wanted/v1/list?page=#{i}")
       all_data << JSON.parse(response_2.body, symbolize_names: true)[:items]
       i += 1
       all_data.flatten!
     end
+    
     all_data
-    binding.pry
   end
 end
