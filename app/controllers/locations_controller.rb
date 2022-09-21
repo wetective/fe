53227a18e0ee4a0dad3f558587389b49f03cefb1
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-21 17:58:25 -0600

    Fix locations map render; update locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,15 @@
 class LocationsController < ApplicationController
   def index
     @locations = Location.all
-    if !@locations.empty?
+    if params[:city]
+      facade = InvestigationFacade.new
       @location = params[:city]
       @investigations = InvestigationFacade.new.cities_investigations(@location)
       # @pagy, @investigations = pagy(@investigations, items: 15)
     end
+  end
+
+  def show
+    @location = Location.find(params[:id])
   end
 end
