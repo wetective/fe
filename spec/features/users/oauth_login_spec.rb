4009c6015be8213ec8dd98686bbf23cc804d491d
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-21 09:24:12 -0400

    Create test for oauth connection

---
+++ spec/features/users/oauth_login_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+
+RSpec.describe "OAuth Login with Google" do 
+  context 'happy path' do
+    before(:each) do
+      OmniAuth.config.test_mode = true
+    end
+    it 'redirects and creates a new user' do
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+                                                                           :provider => 'google',
+                                                                           :uid => '123545',
+                                                                           :info => {
+                                                                             :email => 'me@example.com',
+                                                                             :name => 'Bono'
+                                                                           },
+                                                                           :credentials => {
+                                                                             :token => '555'
+                                                                           }
+                                                                         })
+      visit '/auth/google_oauth2/callback'
+
+      expect(current_path).to eq('/dashboard')
+      expect(User.last.email).to eq('me@example.com')
+    end
+  end
+end
