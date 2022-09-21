3e7207fca62b0f3641d6c56d62ad08e652d5e6f5
Blake Saylor <Blakesaylor@gmail.com>
2022-09-21 10:42:36 -0600

    Remove save and open page

--- spec/features/landing/index_spec.rb
+++ spec/features/landing/index_spec.rb
@@ -59,8 +59,6 @@ RSpec.describe 'The Landing Index Page' do
 
       visit root_path
 
-      save_and_open_page
-
       within '#header-bar' do
         expect(page).to have_css("img[src*='/assets/wetective-logo']")
         expect(page).to have_link('Home', href: root_path)
