aad563699f7fcb1225fc54bd7f53f6fa7077854e
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-21 10:56:22 -0600

    Add tips controller

---
+++ app/controllers/tips_controller.rb
@@ -0,0 +1,35 @@
+class TipsController < ApplicationController
+    def show
+      @tip = TipFacade.get_tip(params[:id])
+    end
+  
+    def new
+      @tip = TipFacade.new
+    end
+  
+    def create
+      @tip = @tip.create_tip(tip_params)
+      if @tip
+        redirect_to :back
+        render :notice, "Your tip was submitted successfully."
+      else
+        redirect_to :new
+        render :alert, "We could not submit your tip at this time. Please check your inputs and try again."
+      end
+    end
+  
+    private
+      def tip_params
+        params.permit(:first_name, :email, :password, :password_confirmation, :authenticity_token, :commit)
+      end
+    
+      def find_tip
+        @tip = Tip.from_google_auth(request.env['omniauth.auth'])
+        if @tip
+          set_session(@tip)
+          redirect_to root_path
+        else
+          redirect_to auth_login_path, notice: "Sorry, your we could not log you in."
+        end
+      end
+  end
