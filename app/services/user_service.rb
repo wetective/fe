5dd3dc2ffa5bacc3bf28dce79e18027cb4dc24de
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-21 09:42:22 -0400

    Edit to ensure correct info being sent to BE. Edit routes to be more RESTful

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -3,27 +3,21 @@ class UserService
         Faraday.new("http://localhost:4999")
     end
 
-    # def self.get_json(response)
-    #     binding.pry
-    #     JSON.parse(response.body, symbolize_names: true)
-    # end
-
-    def self.users
-        response = conn.get("/api/v1/users")
-        get_json(response)
-    end
-
-    def self.user(id)
-        response = conn.get("/api/v1/users/#{id}")
-        get_json(response)
-    end
-
     def self.create_user(data)
         response = conn.post("/api/v1/users/register", {
-            email: data[:email]
+            email: data[:email],
+            name: data[:name]
         }.to_json, "Content-Type" => "application/json")
-        # binding.pry
-        # JSON.parse(response.body, symbolize_names: true)
+        
+        JSON.parse(response.body, symbolize_names: true)
+    end
 
+    def self.find_user(data)
+            response = conn.post("/api/v1/users/find", {
+            email: data[:email],
+            name: data[:name]
+        }.to_json, "Content-Type" => "application/json")
+        
+        JSON.parse(response.body, symbolize_names: true)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'landing#index'
-  get '/login', to: 'landing#login'
-
+  
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
   
+  get '/dashboard', to: 'dashboard#index'
   get '/investigations', to: 'investigations#index'
   get '/investigations/:id', to: 'investigations#show'
+  
+  get '/login', to: 'sessions#login'
+  get '/register', to: 'sessions#register'
+  # post '/register', to: 'sessions#create'
 
-  get '/register', to: 'users#new'
-  post '/register', to: 'users#create'
-
-  get '/login', to: 'sessions#new'
+  # get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   get '/map', to: 'map#index'
 
