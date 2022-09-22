efa0fdd962bd2d8c088d173e3f69fc06647ad18b
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-21 20:48:56 -0600

    Clean up controllers for user and session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,17 +11,32 @@ class SessionsController < ApplicationController
   end
 
   def new
+    find_user
   end
 
   def create
-    user = UserFacade.find_create_user(params)
-    session[:user_id] = user.id
-    redirect_to user_dashboard_path
+    user = find_user
+    if user == nil
+      user = UserFacade.create_user(params)
+      session[:user_id] = user.id
+      redirect_to user_dashboard_path
+    else
+      render :new, notice: "Sorry, your we could not log you in."
+    end
   end
 
   def destroy
     session.destroy
-    flash[:success] = "Successfully Logged Out"
+    render '/', notice: "You have successfully logged out."
     redirect_to root_path
   end
+
+  private
+    def find_user
+      @user = UserFacade.find_user(params[:email])
+    end
+
+    def user_params
+      params.permit(:email, :password)
+    end
 end
