d9ae1274cf58ea5ee026eefb054eb4699bf1bb79
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-22 14:24:10 -0400

    Create ENV for BE API calls that accounts for production vs testing

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -1,24 +1,11 @@
 class UserService
-    # test / development
+    # actual production URL will need to be set in /config/app_environment_variables.rb
     def self.conn
-        Faraday.new("http://localhost:4999")
+        Faraday.new(ENV["DATABASE_URL"])
     end
-    # production
-    # def self.conn
-    #     Faraday.new("production url")
-    # end
 
     def self.create_user(data)
         response = conn.post("/api/v1/users/register", {
-            email: data[:email],
-            name: data[:name]
-        }.to_json, "Content-Type" => "application/json")
-        
-        JSON.parse(response.body, symbolize_names: true)
-    end
-
-    def self.find_user(data)
-            response = conn.post("/api/v1/users/find", {
             email: data[:email],
             name: data[:name]
         }.to_json, "Content-Type" => "application/json")
