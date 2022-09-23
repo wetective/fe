71774a25c595e9faea7aaef5e41547d7ec753528
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-22 20:15:33 -0400

    Edit current user logic, comment out user_controller--unnecessary?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,47 +1,47 @@
-class UsersController < ApplicationController
-  def index
-    @tips = TipFacade.find_tips(params[:id])
-  end
+# class UsersController < ApplicationController
+#   def index
+#     @tips = TipFacade.find_tips(params[:id])
+#   end
   
-  def show
-    @user = UserFacade.get_user(params[:id])
-  end
+#   def show
+#     @user = UserFacade.get_user(params[:id])
+#   end
 
-  def new
-    @user = UserFacade.new
-  end
+#   def new
+#     @user = UserFacade.new
+#   end
 
-  def create
-    user = UserFacade.create_user(user_params)
-    user = UserFacade.find_user(user_params[:email])
-    if user
-      redirect_to user_dashboard_path
-      render :notice, "Welcome, #{@user.first_name}!"
-    else
-      redirect_to auth_login_path
-      render :alert, "We could not create your account at this time. Please check your inputs and try again."
-    end
-  end
+#   def create
+#     user = UserFacade.create_user(user_params)
+#     user = UserFacade.find_user(user_params[:email])
+#     if user
+#       redirect_to user_dashboard_path
+#       render :notice, "Welcome, #{@user.first_name}!"
+#     else
+#       redirect_to auth_login_path
+#       render :alert, "We could not create your account at this time. Please check your inputs and try again."
+#     end
+#   end
 
 
-  private
-    def user_params
-      params.permit(:first_name, :email, :password, :password_confirmation, :authenticity_token, :commit)
-    end
+#   private
+#     def user_params
+#       params.permit(:first_name, :email, :password, :password_confirmation, :authenticity_token, :commit)
+#     end
 
-    def find_user
-      @user = User.from_google_auth(request.env['omniauth.auth'])
-      if @user
-        set_session(@user)
-        redirect_to user_dashboard_path
-      else
-        redirect_to auth_login_path, notice: "Sorry, your we could not log you in."
-      end
-    end
+#     def find_user
+#       @user = User.from_google_auth(request.env['omniauth.auth'])
+#       if @user
+#         set_session(@user)
+#         redirect_to user_dashboard_path
+#       else
+#         redirect_to auth_login_path, notice: "Sorry, your we could not log you in."
+#       end
+#     end
 
-    def set_session(user)
-      session[:user_id] = user.id
-      session[:email] = user.email
-      # session[:image] = user.image
-    end
-end
+#     def set_session(user)
+#       session[:user_id] = user.id
+#       session[:email] = user.email
+#       # session[:image] = user.image
+#     end
+# end
