6625997160d5676b414f83c933d239007490a8fb
Jared Hardinger <jaredhardinger@gmail.com>
2022-09-22 20:16:48 -0400

    Edit to update routing for additional endpoint

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -1,6 +1,15 @@
 class UserFacade 
   def self.find_create_user(data)
-    attributes = UserService.create_user(data)
-    User.new(attributes[:data])
+    attributes = UserService.send_user(data)
+    if attributes[:message]
+        attributes
+    else
+        User.new(attributes[:data])
+    end
+  end
+
+  def self.login(data)
+    attributes = UserService.login_user(data)
+
   end
 end
--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -4,12 +4,21 @@ class UserService
         Faraday.new(ENV["DATABASE_URL"])
     end
 
-    def self.create_user(data)
+    def self.send_user(data)
         response = conn.post("/api/v1/users/register", {
             email: data[:email],
-            name: data[:name]
+            password: data[:password],
+            oauth: data[:oauth]
         }.to_json, "Content-Type" => "application/json")
-        
         JSON.parse(response.body, symbolize_names: true)
     end
+
+    def self.login_user(data)
+        response = conn.post("/api/v1/users/login", {
+            email: data[:email],
+            password: data[:password],
+            oauth: data[:oauth]
+        }.to_json, "Content-Type" => "application/json")
+        JSON.parse(response.body, symbolize_names: true) 
+    end       
 end
